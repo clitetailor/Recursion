578b046c4cbf570ae9b738eb22f3446c627b7f0a
clitetailor <ducnhatphung@gmail.com>
2016-03-06 23:53:55 +0700

    copy 6

--- Recursion.h
+++ Recursion.h
@@ -3,23 +3,23 @@
 
 // Sequence 1: 1, 2, 3, 4, 11, 20, 37, 68, 125, ...
 
-int seq1rec(int para);
-int seq1tailrec(int para, int a0, int a1, int a2);
+int seq1sumrec(int para);
+int seq1sumtailrec(int para, int a0, int a1, int a2);
 
 // Sequence 2: 1, 2, 3, 7, 13, 23, 43, 79, 145, ...
 
-int seq2rec(int para);
-int seq2tailrec(int para, int a0, int a1, int a2, int a3);
+int seq2sumrec(int para);
+int seq2sumtailrec(int para, int a0, int a1, int a2, int a3);
 
 // Sequence 3:
 
-int seq3rec(int para);
-int seq3tailrec(int para, int a0, int a1, int a2, int a3);
+int seq3sumrec(int para);
+int seq3sumtailrec(int para, int a0, int a1, int a2, int a3);
 
 // Sequence 4:
 
-int seq4rec(int para);
-int seq4tailrec(int para, int a0, int a1, int a2, int a3);
+int seq4sumrec(int para);
+int seq4sumtailrec(int para, int a0, int a1, int a2, int a3);
 
 // Sequence 5:
 
